11093032e14d87519fdd5a1ed3ff07305c34cc7f
freedom developer <freedom_developer@163.com>
2026-07-01 23:07:08 +0800

    update

--- module/procfs.c
+++ module/procfs.c
@@ -24,7 +24,7 @@ static int thread_ids_show(struct seq_file *m, void *data)
 	return 0;
 }
 
-static int create_thread_files (struct task_struct *thread, struct proc_dir_entry *thread_dir)
+static int create_thread_proc_files (struct task_struct *thread, struct proc_dir_entry *thread_dir)
 {
 	// 展示各种ID: 
 	if (!proc_create_single_data("ids", 0444, thread_dir, thread_ids_show, (void *)thread)) {
@@ -38,24 +38,44 @@ static int create_thread_files (struct task_struct *thread, struct proc_dir_entr
 static int create_process_proc_files(struct task_struct *process, struct proc_dir_entry *process_dir)
 {
 	int ret;
+	struct task_struct *thread;
+	struct proc_dir_entry *tsk_threads_proc_dir;
 
+
+	if ((ret = create_thread_proc_files(process, process_dir))) 
+		return ret;
 
 	if (atomic_read(&process->signal->live) == 1 || process->flags & PF_KTHREAD)
 		return 0;
 
-	struct proc_dir_entry *tsk_threads_proc_dir = proc_mkdir("", process_dir);
+	
+	tsk_threads_proc_dir = proc_mkdir("threads", process_dir);
 	if (!tsk_threads_proc_dir) {
 		pr_err("create process proc files failed");
 		return -EINVAL;
 	}
 
-	if (create_thread_files(process, process_dir)) {
-
+	for_each_thread(process, thread) {
+		struct proc_dir_entry *thread_dir;
+		char thread_name[16];
+		if (thread == process)
+			continue;
+		snprintf(thread_name, sizeof(thread_name), "%d", task_pid_nr(thread));
+		thread_dir = proc_mkdir(thread_name, tsk_threads_proc_dir);
+		if (!thread_dir) {
+			pr_err("create thread_dir failed\n");
+			return -EINVAL;
+		}
+		if ((ret = create_thread_proc_files(thread, thread_dir))) 
+			return ret;
 	}
+
+	return 0;
 }
 
 int create_procscope_procfs(void)
 {
+	int ret;
 	struct task_struct *process;
 
 	if (procscope_proc_root) {
@@ -70,7 +90,6 @@ int create_procscope_procfs(void)
 	}
 
 	for_each_process(process) {
-		struct task_struct *thread;
 		int tgid = task_tgid_nr(process);
 		char tsk_name[16];
 		struct proc_dir_entry *tsk_proc_dir;
@@ -83,29 +102,9 @@ int create_procscope_procfs(void)
 			return -EINVAL;
 		}
 
-		if (create_process_proc_files(process, tsk_proc_dir)) {
-			pr_err("create process proc files")
-		}
-
-		for_each_thread(process, thread) {
-			struct proc_dir_entry *thread_proc_dir;
-			char thread_name[16];
-
-			snprintf(thread_name, sizeof(thread_name), "%d", task_pid_nr(thread));
-			thread_proc_dir = proc_mkdir(thread_name, tsk_tasks_proc_dir);
-			if (!thread_proc_dir) {
-				pr_err("create proc directory /proc/procscope/%s/tasks/%s failed\n",
-				       tsk_name, thread_name);
-				destroy_procscope_procfs();
-				return -EINVAL;
-			}
-
-			int ret = create_thread_files(thread, thread_proc_dir);
-			if (ret) {
-				destroy_procscope_procfs();
-				return ret;
-			}
-
+		if ((ret = create_process_proc_files(process, tsk_proc_dir))) {
+			pr_err("create process proc files");
+			return ret;
 		}
 	}
 
